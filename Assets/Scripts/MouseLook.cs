e12c04452241e419c6c40b8fb8436a47db04c6d0
juliasherbal <sherbaljulia@gmail.com>
2021-04-18 13:23:10 -0400

    Upload Assets Folder

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 10;
+    Transform playerBody;
+    float pitch = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = transform.parent.transform;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float moveY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+
+        // yaw
+        playerBody.Rotate(Vector3.up * moveX);
+
+        // pitch
+        pitch -= moveY;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float jumpHeight = 10;
+    public float airControl = 10;
+    public float gravity = 9.81f;
+    CharacterController controller;
+    Vector3 input;
+    Vector3 moveDirection;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        if (moveX != 0 || moveY != 0)
+        {
+
+            anim.SetInteger("animState", 1);
+        }
+        else if (moveX == 0 && moveY == 0 && controller.isGrounded)
+        {
+            anim.SetInteger("animState", 0);
+        }
+
+        //input = new Vector3(moveX, 0, moveY);
+        input = (transform.right * moveX + transform.forward * moveY).normalized;
+        input *= speed;
+
+        if (controller.isGrounded)
+        {
+
+            moveDirection = input;
+
+            if (Input.GetButton("Jump"))
+            {
+                Debug.Log("HERE");
+                anim.SetInteger("animState", 2);
+                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+            }
+            else
+            {
+                moveDirection.y = 0.0f;
+            }
+
+        }
+        else
+        {
+            input.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
