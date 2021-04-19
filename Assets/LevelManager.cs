946c0dd3bd4a7d561f06d4cae071d22c22258dab
jmcmanusx47 <jamesmcmanusx47@gmail.com>
2021-04-19 17:21:08 -0400

    timer stuff

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public float levelDuration = 300.0f;
+    public Text timerText;
+    public Text gameText;
+
+    public static bool isGameOver = false;
+
+    float countDown;
+
+
+    void Start()
+    {
+        isGameOver = false;
+        countDown = levelDuration;
+        SetTimerText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameOver)
+        {
+            if (countDown > 0)
+            {
+                countDown -= Time.deltaTime;
+            }
+            else
+            {
+                countDown = 0.0f;
+
+                LevelLost();
+            }
+
+            SetTimerText();
+
+        }
+
+
+    }
+
+
+    void SetTimerText()
+    {
+        timerText.text = countDown.ToString("f2");
+    }
+
+    public void LevelLost()
+    {
+        isGameOver = true;
+        gameText.text = "GAME OVER!";
+        gameText.gameObject.SetActive(true);
+        Invoke("LoadCurrentLevel", 2);
+
+
+    }
+
+    public void LevelBeat()
+    {
+        isGameOver = true;
+        gameText.text = "YOU WIN!";
+        gameText.gameObject.SetActive(true);
+
+    }
+
+    void LoadCurrentLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
