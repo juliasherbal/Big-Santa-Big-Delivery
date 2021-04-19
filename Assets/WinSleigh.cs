58cf4f16e3357e2fdc5e9d34601cf3dae23369b1
jmcmanusx47 <jamesmcmanusx47@gmail.com>
2021-04-19 17:24:20 -0400

    win game

---
+++ Assets/WinSleigh.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinSleigh : MonoBehaviour
+{
+    LevelManager lm;
+    void Start()
+    {
+        lm = GameObject.FindObjectOfType<LevelManager>();
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        lm.LevelBeat();
+    }
+}
